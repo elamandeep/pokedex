e4c12b9ae2d794a9c819dfee9021e8c21706b90e
amandeep2603 <deep.aman1729@gmail.com>
2022-08-13 15:43:29 +0530

    changes in pages

--- pages/pokemons/[id].tsx
+++ pages/pokemons/[id].tsx
@@ -1,11 +1,13 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { fetcher } from "../../../components/helper/fetcher";
-import { Meta } from "../../../components/helper/Meta";
-import { Group, Stack } from "../../../components/style/Flex";
-import { Grid } from "../../../components/style/Grid";
+import { fetcher } from "../../components/helper/fetcher";
+import { Meta } from "../../components/helper/Meta";
+import { Group, Stack } from "../../components/style/Flex";
+import { Grid } from "../../components/style/Grid";
 
 const fetchSinglePokemon = async (id: string | string[] | undefined) => {
   try {
@@ -56,8 +58,8 @@ const index: NextPage = () => {
           })}
         </Group>
 
-        <Grid columns="grid-cols-2" others="w-64 ">
-          <h1 className="font-semibold">Hp</h1>
+        <Grid columns="grid-cols-2" others="w-64 self-center">
+          <h1 className="font-semibold ">Hp</h1>
           <h1 className="font-semibold">{data.data.hp}</h1>
           <h1 className="font-semibold">Attack</h1>
           <h1 className="font-semibold">{data.data.attack}</h1>
@@ -67,7 +69,6 @@ const index: NextPage = () => {
           <h1 className="font-semibold">{data.data.special_attack}</h1>
           <h1 className="font-semibold">Special Defense</h1>
           <h1 className="font-semibold">{data.data.special_defense}</h1>
-
           <h1 className="font-semibold">Speed</h1>
           <h1 className="font-semibold">{data.data.speed}</h1>
         </Grid>
